83b7fc77fbce097376cee9537677bf7264b9cc45
mauri <mauriubal0@gmail.com>
2019-09-19 21:04:50 -0300

    se crea la tabla compra con sus atributos

--- compras/models.py
+++ compras/models.py
@@ -26,3 +26,16 @@ class Producto(models.Model):
         verbose_name = "Producto"
         verbose_name_plural = "Productos"
 
+class Compra(models.Model):
+    Persona = models.ForeignKey(Persona)
+    Producto = models.ForeignKey(Producto)
+    Fecha = models.DateField()
+    Hora = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '%s compro %s' % (self.Persona, self.Producto)
+
+    class Meta:
+        verbose_name = "Compra"
+        verbose_name_plural = "Compras"
+
