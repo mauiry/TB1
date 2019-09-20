e25be4636d8ab83bcf3470a788fc4faaf7eebe3b
mauri <mauriubal0@gmail.com>
2019-09-19 21:09:27 -0300

    se importan los objetos persona,compra y producto y se registran

---
+++ compras/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from models import Persona, Producto, Compra
+# Register your models here.
+admin.site.register(Persona)
+
+admin.site.register(Producto)
+
+admin.site.register(Compra)
+
