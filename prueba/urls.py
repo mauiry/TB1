76847ea41f4c7e92ec854a347d5fb71792bf7f91
mauri <mauriubal0@gmail.com>
2019-09-19 21:32:13 -0300

    se agregan urls de las diferentes vistas compra,producto y persona

--- prueba/urls.py
+++ prueba/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+
+from app import views
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'prueba.views.home', name='home'),
+    # url(r'^$', 'pract.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^personas/',views.listarPersona),
+    url(r'^productos/',views.listarProducto),
+    url(r'^compras/',views.listarCompra),
 )
+
